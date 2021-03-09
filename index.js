fa29cd722f6cd86bac2b31d2652513ec4d688d9f
Msaad668 <mostafasaad55@gmail.com>
2021-03-09 21:51:26 +0200

    added docker file

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+
+app.get("/hello", (req, res) => {
+  res.send("hello world");
+});
 
 app.listen(3000, () => console.log("server running on port 3000"));
